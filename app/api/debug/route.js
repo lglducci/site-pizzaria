79faaa34fa036eec75cf1032a14f07b9231acc73
lglducci <lglducci@hotmail.com>
2025-08-25 10:49:17 -0300

    Delete app directory

--- app/api/debug/route.js
+++
@@ -1,6 +0,0 @@
-export async function GET() {
-  return Response.json({
-    NEXT_PUBLIC_N8N_MENU_URL: process.env.NEXT_PUBLIC_N8N_MENU_URL || null,
-    N8N_MENU_URL: process.env.N8N_MENU_URL || null,
-  });
-}
