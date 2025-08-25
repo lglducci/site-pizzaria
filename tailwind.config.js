3bd3f72a8f85218a41fd524517e1ffb2de1fe990
lglducci <lglducci@hotmail.com>
2025-08-25 12:58:22 -0300

    ok

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./app/**/*.{js,jsx}", "./components/**/*.{js,jsx}", "./pages/**/*.{js,jsx}", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
-  plugins: [],
-};
