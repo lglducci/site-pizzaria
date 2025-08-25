d8e7b5561adcd2600d5be34bcf85a7c474578eb1
lglducci <lglducci@hotmail.com>
2025-08-25 19:06:18 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ function pickArray(x){
     for(const k of keys) if(Array.isArray(x[k])) return x[k];
     const anyArr=Object.values(x).find(v=>Array.isArray(v)); if(anyArr) return anyArr;
   }
-  return [x];
+  return [x]; 
 }
 export async function getServerSideProps(){
   try{
@@ -201,4 +201,5 @@ export default function Home({ menu, error }){
     </main>
   );
 }
+
 
