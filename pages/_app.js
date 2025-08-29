696de9ada9bda537ee5cf2ae232d82d8e83fa2df
lglducci <lglducci@hotmail.com>
2025-08-29 19:02:47 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
  // pages/_app.js
 import '../styles/globals.css'; // pode remover se não usar
-import { CartProvider } from '../CartContext'; // <-- seu CartContext está na RAIZ do repo
+//import { CartProvider } from '../CartContext'; // <-- seu CartContext está na RAIZ do repo
+import { CartProvider } from '../context/CartContext';
 
 export default function MyApp({ Component, pageProps }) {
   return (
