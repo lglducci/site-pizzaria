4fba87c3e6e32c3ceed20756b5be07cf2562b496
lglducci <lglducci@hotmail.com>
2025-08-29 09:43:40 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
- // next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+ 
 
-module.exports = nextConfig
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true }, // se não usa TS, não afeta
+};
+module.exports = nextConfig;
