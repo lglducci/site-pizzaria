f233d17a0c0aec297057edfeabb5d460e625540e
lglducci <lglducci@hotmail.com>
2025-08-29 18:30:18 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,21 @@
+
+
+
+
 import '../styles/globals.css';
 import '../styles.css';
 console.log('APP carregado');
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
+}
+// pages/_app.js
+import '../styles/globals.css'; // se você não tiver, pode remover esta linha
+import { CartProvider } from '../components/CartContext'; // <— ajuste o caminho se seu CartContext estiver em outro lugar
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <CartProvider>
+      <Component {...pageProps} />
+    </CartProvider>
+  );
 }
