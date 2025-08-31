0c11ccd496a9d1dc33adc953012e631756561fa8
lglducci <lglducci@hotmail.com>
2025-08-31 15:42:12 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,18 @@
+// pages/_app.js
+import { useEffect } from 'react';
+import { migrateCartCategories } from '../lib/cart';
+// (se você tiver globals.css, deixe a importação; se não tiver, pode remover)
 import '../styles/globals.css';
-import '../styles.css';
-console.log('APP carregado');
-export default function App({ Component, pageProps }) {
+
+export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Corrige carrinhos antigos SEM categoria
+    migrateCartCategories();
+  }, []);
+
   return <Component {...pageProps} />;
 }
+
+
+
+ 
